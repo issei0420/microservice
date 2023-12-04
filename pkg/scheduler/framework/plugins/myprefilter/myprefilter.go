aafa7850424358b9f278c80c027e84c2e44d652d
Issei Saiki <i.saiki@aster-ai.com>
2023-12-04 23:47:26 +0900

    map作成まで

--- pkg/scheduler/framework/plugins/myprefilter/myprefilter.go
+++ pkg/scheduler/framework/plugins/myprefilter/myprefilter.go
@@ -2,7 +2,11 @@ package myprefilter
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -11,7 +15,9 @@ import (
 )
 
 // MyPreFilterPlugin は PreFilterPlugin インターフェースを実装するカスタムプラグインです。
-type MyPreFilterPlugin struct{}
+type MyPreFilterPlugin struct {
+	ServiceScores map[string]int64
+}
 
 // インターフェースの実装を確実にするために、コンパイル時の型チェックを行います。
 var _ framework.PreFilterPlugin = &MyPreFilterPlugin{}
@@ -21,38 +27,88 @@ func (pl *MyPreFilterPlugin) Name() string {
 	return "MyPreFilterPlugin"
 }
 
+// PrometheusResponse は Prometheus API のレスポンス構造を表します。
+type PrometheusResponse struct {
+	Status string `json:"status"`
+	Data   struct {
+		ResultType string `json:"resultType"`
+		Result     []struct {
+			Metric struct {
+				App string `json:"app"` // レスポンスに合わせて `Service` から `App` に変更
+			} `json:"metric"`
+			Value [2]interface{} `json:"value"`
+		} `json:"result"`
+	} `json:"data"`
+}
+
 // PreFilter はプラグインのメインロジックです。
 func (pl *MyPreFilterPlugin) PreFilter(ctx context.Context, state *framework.CycleState, p *v1.Pod) (*framework.PreFilterResult, *framework.Status) {
 	klog.Infof("yah! yah! yah! from myprefilter\n")
 	// ポッドのラベルからサービス名を取得する
 	sn := p.Labels["app"]
-	if sn != "" {
-		klog.Infof("PreFilter called for service: %s", sn)
-	} else {
-		klog.Info("PreFilter called for a pod without an 'app' label")
+	if sn == "" {
+		klog.Info("MyPreFilter called for a pod without an 'app' label")
+		return nil, framework.NewStatus(framework.Error, "Pod does not have an 'app' label")
 	}
-	// ここに必要なロジックを実装します。
-	// この例では、特に PreFilterResult を変更する必要はないので nil を返します。
 
-	// api通信
-	// Prometheus サービスへのクエリを実行
-	queryURL := "http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090/api/v1/query?query=up"
+	// Prometheus クエリの構築
+	baseURL := "http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090/api/v1/query"
+	queryParams := fmt.Sprintf("topk(5, sum(rate(request_total{app=\"%s\", namespace=\"saiki\", dst_service!=\"redis-cart\", dst_service!=\"otel-collector-collector\", dst_service!=\"jaeger\", dst_service!=\"prometheus-kube-prometheus-prometheus\", dst_service!=\"\"}[60m])) by (dst_service) OR sum(rate(request_total{dst_service=\"%s\", namespace=\"saiki\", app!=\"redis-cart\", app!=\"otelcollector\", app!=\"jaeger\", app!=\"\"}[60m])) by (app))", sn, sn)
+
+	// クエリ文字列のエンコード
+	encodedQuery := url.QueryEscape(queryParams)
+	queryURL := baseURL + "?query=" + encodedQuery
+
+	// クエリ文字列のログ出力
+	klog.Info("Prometheus query: ", queryURL)
+
+	// HTTPリクエストの実行
 	resp, err := http.Get(queryURL)
 	if err != nil {
-		// HTTP エラーをハンドル
 		return nil, framework.NewStatus(framework.Error, err.Error())
 	}
 	defer resp.Body.Close()
 
-	// レスポンスの内容を読み込む
-	// body, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	// 読み込みエラーをハンドル
-	// 	return nil, framework.NewStatus(framework.Error, err.Error())
-	// }
+	// HTTPステータスコードのチェック
+	if resp.StatusCode != http.StatusOK {
+		return nil, framework.NewStatus(framework.Error, "Received non-OK response from Prometheus: "+resp.Status)
+	}
 
-	// 応答をログに出力
-	// klog.Info(string(body))
+	// レスポンスの内容を読み込む
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, framework.NewStatus(framework.Error, err.Error())
+	}
+
+	// レスポンスの内容をログに出力
+	klog.Info("Prometheus response: ", string(body))
+
+	// Prometheusレスポンスの解析
+	var result PrometheusResponse
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, framework.NewStatus(framework.Error, err.Error())
+	}
+
+	// サービススコアのマップを作成
+	pl.ServiceScores = make(map[string]int64)
+	score := int64(50)
+	for i, res := range result.Data.Result {
+		if i >= 5 {
+			break
+		}
+		// `Service` ではなく `App` を使用
+		pl.ServiceScores[res.Metric.App] = score
+		score -= 10
+	}
+
+	if len(pl.ServiceScores) == 0 {
+		return nil, framework.NewStatus(framework.Error, "No services found in Prometheus response")
+	}
+
+	// マップの内容をログに出力
+	for service, score := range pl.ServiceScores {
+		klog.Infof("Service: %s, Score: %d", service, score)
+	}
 
 	return nil, framework.NewStatus(framework.Success, "")
 }
@@ -65,5 +121,5 @@ func (pl *MyPreFilterPlugin) PreFilterExtensions() framework.PreFilterExtensions
 
 // New は新しいプラグインのインスタンスを作成します。
 func New(ctx context.Context, configuration runtime.Object, f framework.Handle) (framework.Plugin, error) {
-	return &MyPreFilterPlugin{}, nil
+	return &MyPreFilterPlugin{ServiceScores: make(map[string]int64)}, nil
 }
--- pkg/scheduler/framework/plugins/myscore/myscore.go
+++ pkg/scheduler/framework/plugins/myscore/myscore.go
@@ -2,10 +2,6 @@ package myscore
 
 import (
 	"context"
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-	"net/url"
 
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -19,16 +15,6 @@ var _ framework.ScorePlugin = &MyScorePlugin{}
 // MyScorePlugin is a sample Score plugin that applies some custom logic.
 type MyScorePlugin struct{}
 
-type PrometheusResponse struct {
-	Status string `json:"status"`
-	Data   struct {
-		ResultType string `json:"resultType"`
-		Result     []struct {
-			Value [2]interface{} `json:"value"`
-		} `json:"result"`
-	} `json:"data"`
-}
-
 // Name returns the name of the plugin.
 func (pl *MyScorePlugin) Name() string {
 	return "MyScorePlugin"
@@ -39,48 +25,12 @@ func (pl *MyScorePlugin) Name() string {
 func (pl *MyScorePlugin) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
 	klog.Infof("yeah! yeah! yeah! from myscore\n")
 
-	// Prometheus クエリの構築
-	baseURL := "http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090/api/v1/query"
-	queryParams := "sum(rate(request_total{app=\"frontend\",dst_service=\"adservice\", namespace=\"saiki\"}[60m]))"
-
-	// クエリ文字列のエンコード
-	encodedQuery := url.QueryEscape(queryParams)
-	queryURL := baseURL + "?query=" + encodedQuery
-
-	// HTTPリクエストの実行
-	resp, err := http.Get(queryURL)
-	if err != nil {
-		return 0, framework.NewStatus(framework.Error, err.Error())
-	}
-	defer resp.Body.Close()
-
-	// HTTPステータスコードのチェック
-	if resp.StatusCode != http.StatusOK {
-		return 0, framework.NewStatus(framework.Error, "Received non-OK response from Prometheus: "+resp.Status)
-	}
-
-	// レスポンスの内容を読み込むg
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return 0, framework.NewStatus(framework.Error, err.Error())
-	}
-	// 応答の内容をログに出力
-	klog.Info("Prometheus response: ", string(body))
-
-	var result PrometheusResponse
-	if err := json.Unmarshal(body, &result); err != nil {
-		return 0, framework.NewStatus(framework.Error, err.Error())
-	}
-
-	// Result 配列が空でないことを確認
-	if len(result.Data.Result) == 0 {
-		klog.Info("No data in Prometheus response")
+	// Pod のサービス名を取得
+	serviceName := pod.Labels["app"]
+	if serviceName == "" {
+		klog.Info("Pod does not have an 'app' label")
 		return 0, framework.NewStatus(framework.Success)
 	}
-
-	// 変化率を取り出し、ログに出力
-	changeRate := result.Data.Result[0].Value[1]
-	klog.Infof("Change rate: %v\n", changeRate)
 
 	return 50, framework.NewStatus(framework.Success)
 }
